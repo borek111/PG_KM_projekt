528e7f6982b77b011a07dc4f4263268e77fee9c7
borek111 <przemekgrabecki@gmail.com>
2024-12-16 21:02:03 +0100

    dodanie podglądu na żywo kursów i szans

--- js/kostki.js
+++ js/kostki.js
@@ -4,8 +4,8 @@ let wynikKostki = 0;
 let liczbaRzutow = 0;
 var los;
 var losSTRING;
-var kostka1PNG=document.getElementById("kostka1_png");
-var kostka2PNG=document.getElementById("kostka2_png");
+var kostka1PNG = document.getElementById("kostka1_png");
+var kostka2PNG = document.getElementById("kostka2_png");
 var zakresMax1;
 var zakresMin1;
 var roznica; 
@@ -15,7 +15,7 @@ var szanse_text = document.getElementById("szanse");
 var wygrana_text = document.getElementById("wygrana");
 var wygrana;
 
-//suwak z jquery
+// Suwak z jQuery
 $(function() {
     $("#suwak").slider({
         range: true,           
@@ -24,42 +24,42 @@ $(function() {
         values: [1, 6],         
         slide: function(event, ui) {
             aktualizujZakres(ui.values);
-            //obliczMnoznik(); to do zeby byl aktualny zakres a nie -1 
+            obliczMnoznik(ui.values);  
+            obliczSzanse(ui.values);   
         },
         stop: function(event, ui) {
-            aktualizujZakres(ui.values); 
-            obliczMnoznik();
-            obliczSzanse();
-            
+            aktualizujZakres(ui.values);
+            obliczMnoznik(ui.values);
+            obliczSzanse(ui.values);
         }
     });
 
     aktualizujZakres($("#suwak").slider("values"));
+    obliczMnoznik($("#suwak").slider("values"));
+    obliczSzanse($("#suwak").slider("values"));
 });
-
 
 function aktualizujZakres(zakres) {
     const zakresMin = document.getElementById("zakres-min");
-    zakresMin1=zakresMin.textContent;
     const zakresMax = document.getElementById("zakres-max");
-    zakresMax1=zakresMax.textContent;
 
-    // Ustawianie wartości minimalnej i maksymalnej
     zakresMin.textContent = zakres[0];
     zakresMax.textContent = zakres[1];
+
+    zakresMin1 = zakres[0];
+    zakresMax1 = zakres[1];
 }
 
 function startGry() {
-    // if (graRozpoczeta) {
-    //     return;
-    // }
     mnoznik = 1;
     aktualizujZakres($("#suwak").slider("values"));
     Losowanie();
     UstawKostke2();
     UstawKostke1();
     sprawdzWynik();
+
     stawka = parseInt(document.getElementById("stawka").value);
+    console.log("Aktualna stawka: " + stawka);
 
     if (isNaN(stawka) || stawka <= 0) {
         alert("Proszę podać poprawną stawkę.");
@@ -67,6 +67,8 @@ function startGry() {
     }
 
     let srodki = getSrodki(); 
+    console.log("Aktualne środki: " + srodki);
+
     if (stawka > srodki) {
         alert("Nie masz wystarczających środków!");
         return;
@@ -75,123 +77,80 @@ function startGry() {
     srodki -= stawka; 
     setSrodki(srodki); 
     updateSrodkiWyswietlane(); 
-
-    //graRozpoczeta = true;
-    liczbaRzutow = 0; 
-
-    
-    // document.querySelector('button[type="submit"]').disabled = true;
-
-    
 }
 
 function zwrotPieniedzy() {
-    // if (!graRozpoczeta) {
-    //     alert("Gra nie została rozpoczęta.");
-    //     return;
-    // }
-    obliczMnoznik();
-    stawka = parseInt(document.getElementById("stawka").value);
-    console.log("stawka"+stawka);
-    console.log("mno"+mnoznik);
-    let srodki = getSrodki();
-    wygrana = stawka * mnoznik;
-    srodki += (stawka * mnoznik);
-    setSrodki(srodki); 
-    updateSrodkiWyswietlane();
-   
+    if (los >= zakresMin1 && los <= zakresMax1) {
+        stawka = parseInt(document.getElementById("stawka").value);
+        console.log("stawka: " + stawka);
+        console.log("mnożnik: " + mnoznik);
+        
+        let srodki = getSrodki();
+        wygrana = stawka * mnoznik;
+        srodki += wygrana;
+        setSrodki(srodki); 
+        updateSrodkiWyswietlane();
+        wygrana_text.innerHTML = "Ostatnia wygrana: " + wygrana.toFixed(2);
+    } else {
+        wygrana_text.innerHTML = "Niestety przegrałeś :(";
+    }
 }
 
-function getRoznica()
-{
+function getRoznica() {
     roznica = zakresMax1 - zakresMin1;
     return roznica;
 }
 
-function obliczMnoznik()
-{
+function obliczMnoznik(zakres) {
     getRoznica();
     switch (roznica) {
-        case 0:
-            mnoznik = 3;
-            mnoznik_text.innerHTML = "Mnożnik:<br>"+mnoznik;
-            break;
-        case 1:
-            mnoznik = 2.5;
-            mnoznik_text.innerHTML = "Mnożnik:<br>"+ mnoznik;
-            break;
-        case 2:
-            mnoznik = 2;
-            mnoznik_text.innerHTML = "Mnożnik:<br>"+ mnoznik;
-            break;
-        case 3:
-            mnoznik = 1.5;
-            mnoznik_text.innerHTML = "Mnożnik:<br>"+mnoznik;
-            break;
-        case 4:
-            mnoznik = 1.25;
-            mnoznik_text.innerHTML = "Mnożnik:<br>"+ mnoznik;
-            break;
-        case 5:
-            mnoznik = 1;
-            mnoznik_text.innerHTML =  "Mnożnik:<br>"+mnoznik;
-            break
-        default:
-            mnoznik = 1;
-            mnoznik_text.innerHTML =  "Mnożnik:<br>"+mnoznik;
-      }
+        case 0: mnoznik = 3; break;
+        case 1: mnoznik = 2.5; break;
+        case 2: mnoznik = 2; break;
+        case 3: mnoznik = 1.5; break;
+        case 4: mnoznik = 1.25; break;
+        case 5: mnoznik = 1; break;
+        default: mnoznik = 1;
+ }
+
+    document.getElementById("mnoznik").innerHTML = "Mnożnik:<br>" + mnoznik;
 }
 
-function obliczSzanse()
-{
+function obliczSzanse(zakres) {
     getRoznica();
+
     switch (roznica) {
-        case 0:
-            szanse_text.innerHTML = "Szanse:<br>" + 16.6 + "%";
-            break;
-        case 1:
-            szanse_text.innerHTML = "Szanse:<br> " + 33.3 + "%";
-            break;
-        case 2:
-            szanse_text.innerHTML = "Szanse:<br> " + 50 + "%";
-            break;
-        case 3:
-            szanse_text.innerHTML = "Szanse:<br> " + 66.6 + "%";
-            break;
-        case 4:
-            szanse_text.innerHTML = "Szanse:<br> " + 83.6 + "%";
-            break;
-        case 5:
-            szanse_text.innerHTML = "Szanse:<br> " + 100 + "%";
-            break
-        default:
-            szanse_text.innerHTML = "Szanse:<br> " + 100 + "%";
-      }
+        case 0: szanse = "16.6%"; break;
+        case 1: szanse = "33.3%"; break;
+        case 2: szanse = "50%"; break;
+        case 3: szanse = "66.6%"; break;
+        case 4: szanse = "83.3%"; break;
+        case 5: szanse = "100%"; break;
+        default: szanse = "100%";
+    }
+
+    document.getElementById("szanse").innerHTML = "Szanse:<br>" + szanse;
 }
 
-function Losowanie(){
-    los=Math.floor(Math.random() * 6)+1;
+function Losowanie() {
+    los = Math.floor(Math.random() * 6) + 1;
     losSTRING = String(los);
 }
-function UstawKostke1(){
-    kostka1PNG.src="../grafika/kostki/"+losSTRING+".png";
-}
-function UstawKostke2(){
-    kostka2PNG.src=kostka1PNG.src;
+
+function UstawKostke1() {
+    kostka1PNG.src = "../grafika/kostki/" + losSTRING + ".png";
 }
 
+function UstawKostke2() {
+    kostka2PNG.src = kostka1PNG.src;
+}
 
-function sprawdzWynik(){
+function sprawdzWynik() {
     console.log(zakresMin1, zakresMax1);
     console.log(los);
-    if(los>=zakresMin1&&los<=zakresMax1)
-    {
+    if (los >= zakresMin1 && los <= zakresMax1) {
         zwrotPieniedzy(); 
-        wygrana_text.innerHTML = "Ostatnia wygrana: "+ wygrana.toFixed(2);
-    }
-    else
-    {
+    } else {
         wygrana_text.innerHTML = "Niestety przegrałeś :(";
     }
-
 }
